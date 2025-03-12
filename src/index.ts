b6afed645eeeb9dd17671402c78bc02708963611
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-12 16:31:00 -0300

    fea: setup hono

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,11 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
 
-export default {
-  async fetch(request, env, ctx): Promise<Response> {
-    console.log(request.cf);
-    return new Response(JSON.stringify({ hello: "World" }), {
-      headers: { "content-type": "application/json" },
-    });
-  },
-} satisfies ExportedHandler<Env>;
+export interface Env { }
+
+const app = new Hono<Env>();
+
+app.get("/", c => {
+	return c.json({ hello: "World" });
+});
+
+export default app satisfies ExportedHandler<Env>;
