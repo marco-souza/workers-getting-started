e6ede9660bfc4797c979354f73e24707047fda0d
Marco Antônio <ma.souza.junior@gmail.com>
2025-03-12 17:25:36 -0300

    fea: setup biome as formatter

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # CLoudflare Workers - Getting Started
 
-This is a experimental repository to explore Cloudflare Workers, AI bindings, and other integrations
+This is a experimental repository to explore Cloudflare Workers, AI bindings,
+and other integrations
 
-The app is available at: https://workers-getting-started.ma-souza-junior.workers.dev/
+The app is available at:
+https://workers-getting-started.ma-souza-junior.workers.dev/
 
 > [!NOTE]
 > Inspired by
+>
 > - Cloudflare Workers 101: https://youtu.be/H7Qe96fqg1M?si=xjhWD3TB6FIt1rSW
 > - Cloudflare Workers 201: https://youtu.be/QTsaAhFvX9o?si=c2P0d4uk40mbjBWu
 
-
 > [!WARNING]
-> Tests are failing - related issue https://github.com/cloudflare/workers-sdk/issues/6796
+> Tests are failing - related issue
+> https://github.com/cloudflare/workers-sdk/issues/6796
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,46 @@
 import { Ai } from "@cloudflare/ai";
 import { Hono } from "hono";
 
-export interface Env {
-	AI: Ai
+export interface Bindings {
+	AI: Ai;
+	CACHE: KVNamespace;
 }
 
-const app = new Hono<{ Bindings: Env }>();
+const app = new Hono<{ Bindings: Bindings }>();
 
-app.get("/", async c => {
-	const query = c.req.query()
-
+app.get("/", async (c) => {
+	const query = c.req.query();
 
 	const askAi = query.ai === "true";
-	console.log({ query, askAi })
+	console.log({ query, askAi });
 
 	if (!askAi) {
-		console.log("no ai")
+		console.log("no ai");
 		return c.json({ hello: "World" });
 	}
 
-
-	console.log("[ai]")
+	console.log("[ai]");
 	const ai = new Ai(c.env.AI);
 
 	const messages = [
-		{ role: "system", content: "Hello,You are a friendly assistent to generate greetings for our users" },
-		{ role: "user", content: "Hello, I am a user" }
+		{
+			role: "system",
+			content:
+				"Hello,You are a friendly assistent to generate greetings for our users",
+		},
+		{ role: "user", content: "Hello, I am a user" },
 	];
 
-	const inputs = { messages }
-	const res = await ai.run("@cf/meta/llama-3-8b-instruct", inputs)
+	const inputs = { messages };
+	const res = await ai.run("@cf/meta/llama-3-8b-instruct", inputs);
 
 	return c.json(res);
 });
 
-app.get("/:username", async c => {
+app.get("/:username", async (c) => {
 	const username = c.req.param("username");
 
-	console.log({ username })
+	console.log({ username });
 
 	const res = await fetch(`https://api.github.com/users/${username}/repos`, {
 		headers: {
@@ -45,9 +48,9 @@ app.get("/:username", async c => {
 		},
 	});
 	const repos = await res.json();
-	const { length: count } = repos as unknown[]
+	const { length: count } = repos as unknown[];
 
 	return c.json({ repos, count });
-})
+});
 
-export default app satisfies ExportedHandler<Env>;
+export default app satisfies ExportedHandler<Bindings>;
